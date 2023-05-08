58a229c71aef5f1cc12e2b3aee14698b129b6ef1
morhaf <morhafg366@gmail.com>
2023-05-08 16:14:37 +0200

    adding the SQL connection

--- ProNatur-Biomarkt GmbH/ProductScreen.cs
+++ ProNatur-Biomarkt GmbH/ProductScreen.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,8 @@ namespace ProNatur_Biomarkt_GmbH
 {
     public partial class ProductScreen : Form
     {
+        private SqlConnection dataBaseConnection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\Morhaf.Haedar\Documents\Pro-Natur Biomarkt GmbH.mdf;Integrated Security = True; Connect Timeout = 30");
+
         public ProductScreen()
         {
             InitializeComponent();
